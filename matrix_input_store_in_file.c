be1731144d3d213a1f317be5b0f4710c562460c0
Faruqui <30295498+Faruqui@users.noreply.github.com>
2025-02-23 17:04:44 +0600

    Update and rename matrix_input.c to matrix_input_store_in_file.c

--- matrix_input_store_in_file.c
+++ matrix_input_store_in_file.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
-  Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <math.h>
 
@@ -40,7 +32,7 @@ void readMatrixAndStore(int row, int col, int mat[row][col]){
             int radius = mat[r][c];
             float area = M_PI * mat[r][c] * mat[r][c];
             float parameter = 2 * M_PI * mat[r][c];
-            fprintf(fp, "b)%d\tc) %f\t d) %f\t \n", radius, area, parameter);
+            fprintf(fp, "b)Radius: %d\tc)Area: %f\t d)Perimeter %f\t \n", radius, area, parameter);
             serial++;
         }
         
@@ -51,8 +43,8 @@ void readMatrixAndStore(int row, int col, int mat[row][col]){
 
 int main()
 {   
-    int row = 2;
-    int col = 2;
+    int row = 5;
+    int col = 5;
     int mat[row][col];
     generateMatrix(row, col, mat);
     printMatrix(row, col, mat);
